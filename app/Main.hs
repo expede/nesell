355e1d0fa28ba6fc8d86562c38fe031127da8c71
Brooklyn Zelenka <be.zelenka@gmail.com>
2016-12-19 22:23:02 -0800

    Absoluet basics (Cabal file & resolver)

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Lib
+import Foundation
+import Nesell
 
 main :: IO ()
 main = someFunc
--- src/Nesell.hs
+++ src/Nesell.hs
@@ -1,6 +1,10 @@
-module Lib
+{-# LANGUAGE OverloadedStrings #-}
+
+module Nesell
     ( someFunc
     ) where
+
+import Foundation
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
